7b3b0e367d4a2c098ab0b2bca3cfba1bfb1e6655
Joel Nabda <joelnabda42@gmail.com>
2026-01-07 13:34:31 +0000

    D-CLIC_semaine_4

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MonAppli());
 }
+
 class MonAppli extends StatelessWidget {
   const MonAppli({super.key});
 
@@ -15,6 +16,7 @@ class MonAppli extends StatelessWidget {
     );
   }
 }
+
 class pageAccueil extends StatelessWidget {
   const pageAccueil({super.key});
 
@@ -36,10 +38,23 @@ class pageAccueil extends StatelessWidget {
           ),
         ],
       ),
-     body: Center(
-  child: Image.asset('assets/images/magazineInfo.jpg'),
-),
-floatingActionButton: FloatingActionButton(
+      // Modification : Utilisation de SingleChildScrollView pour rendre la page scrollable
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            // Image du magazine avec BoxFit pour l'adapter à l'écran
+            Image(
+              image: AssetImage('assets/images/magazineInfo.jpg'),
+              fit: BoxFit.cover,  // Ajuste l'image pour qu'elle ne déborde pas
+            ),
+            PartieTitre(),
+            PartieTexte(),
+            PartieIcone(),
+            PartieRubrique(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple,
         onPressed: () {
           ScaffoldMessenger.of(context).showSnackBar(
@@ -50,124 +65,115 @@ floatingActionButton: FloatingActionButton(
         },
         child: const Text('Click'),
       ),
-
     );
   }
 }
 
+// PartieTitre : Classe pour afficher les titres
+class PartieTitre extends StatelessWidget {
+  const PartieTitre({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return Container(
+      child: Column(
+        children: const [
+          Text(
+            "Titre du premier niveau",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Titre du second niveau",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// PartieTexte : Classe pour afficher une brève description du magazine
+class PartieTexte extends StatelessWidget {
+  const PartieTexte({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      child: const Text(
+        "Ceci est une brève description du magazine.",
+        style: TextStyle(fontSize: 16),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+}
+
+// PartieIcone : Classe pour afficher les icônes (téléphone, e-mail, partage)
+class PartieIcone extends StatelessWidget {
+  const PartieIcone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const [
+          Column(
+            children: [
+              Icon(Icons.phone),
+              SizedBox(height: 5),
+              Text("TEL"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.email),
+              SizedBox(height: 5),
+              Text("MAIL"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.share),
+              SizedBox(height: 5),
+              Text("PARTAGE"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// PartieRubrique : Classe pour afficher les images côte à côte avec des bords arrondis
+class PartieRubrique extends StatelessWidget {
+  const PartieRubrique({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            child: Image(
+              image: AssetImage('assets/images/rubriq1.jpg'),
+              width: 150,   // Augmenter la taille pour tester
+              height: 150,  // Augmenter la taille pour tester
+              fit: BoxFit.cover,  // Cela ajuste l'image pour remplir l'espace sans la déformer
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            child: Image(
+              image: AssetImage('assets/images/rubriq2.jpg'),
+              width: 150,   // Augmenter la taille pour tester
+              height: 150,  // Augmenter la taille pour tester
+              fit: BoxFit.cover,  // Cela ajuste l'image pour remplir l'espace sans la déformer
+            ),
+          ),
+        ],
       ),
     );
   }
